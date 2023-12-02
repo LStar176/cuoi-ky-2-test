24877689e38e6d5bb4e61f94b8effc110351c004
LStar176 <baoduy.d17@gmail.com>
2023-12-02 23:59:23 +0700

    Update Active.jsx

--- src/components/Active.jsx
+++ src/components/Active.jsx
@@ -6,19 +6,11 @@ import { Context } from './context'
 
 const Active = () => {
     const { todos, setTodos } = useContext(Context)
-<<<<<<< HEAD
     const dataFilter = todos.filter((todo) => {
         return todo.status === 'active'
     })
 
 
-=======
-    console.log(todos);
-    const dataFilter = todos.filter((todo) => {
-        return todo.status === 'active'
-    })
-    console.log(dataFilter);
->>>>>>> e6c08350431b48ff9b98acfab65405f265d31a09
     return (
         <>
             <InputTodo />
